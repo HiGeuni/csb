8d57c963158c85b93cf7a321678657676d4ee9ad
HiGeuni <khyogeun_@naver.com>
2024-01-20 14:15:52 +0900

    Style: Develop UI

--- src/pages/help/index.tsx
+++ src/pages/help/index.tsx
@@ -2,60 +2,45 @@ import React from 'react';
 import { HelpMenus } from '@src/assets/menus';
 import TempLayout from '@src/components/Layout/TabLayout/TempLayout';
 import { motion } from 'framer-motion';
-import { FaPaperPlane } from 'react-icons/fa';
+import { FaEnvelope, FaPaperPlane } from 'react-icons/fa';
+import LocationMap from '@src/components/Help/LocationMap';
+import { FaLocationDot, FaMessage } from 'react-icons/fa6';
 
 const HelpMain = () => {
   return (
     <TempLayout title='고객 지원' detailTitle='Help' menus={HelpMenus}>
-      <div className='text-3xl font-medium capitalize mb-8 text-center'>Contact Me</div>
-      <div className='flex justify-center mt-8 px-4'>
-        <motion.section
-          id='contact'
-          className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center'
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{
-            duration: 1,
-          }}
-          viewport={{
-            once: true,
-          }}
-        >
-          <p className='text-primary -mt-6'>
-            Please contact me directly at{' '}
-            <a className='underline text-accent' href='malito:example@gmail.com'>
-              pennbit.korea@gmail.com
-            </a>{' '}
-            or through this form.
-          </p>
-          <form className='mt-10 flex flex-col'>
-            <input
-              className='h-14 px-4 rounded-lg borderBlack'
-              name='senderEmail'
-              type='email'
-              required
-              maxLength={500}
-              placeholder='Your email'
-            />
-            <textarea
-              className='h-52 my-3 rounded-lg borderBlack p-4'
-              name='message'
-              required
-              maxLength={500}
-              placeholder='Your message'
-            />
-            <div className='w-full flex justify-end mt-4'>
-              <button
-                className='group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-accent text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105'
-                type='submit'
-              >
-                Submit{' '}
-                <FaPaperPlane className='text-xs opacity-70 transition-all group-hover:translate-x-1 group-hover:-translate-y-1' />
-              </button>
-            </div>
-          </form>
-        </motion.section>
-      </div>
+      <div className='ml-4 text-3xl font-medium capitalize mb-2 text-center md:text-start'>Contact Me</div>
+      <p className='ml-4 text-primary text-center md:text-start'>문의 사항이 있다면 아래의 정보로 연락 주세요.</p>
+      <motion.section
+        id='contact'
+        className='w-full flex-col flex md:flex-row justify-between gap-x-20 items-center px-4 '
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          duration: 1,
+        }}
+        viewport={{
+          once: true,
+        }}
+      >
+        <div className='mt-10 md:m-0 flex flex-col md:justify-center md:gap-12 w-full md:w-[400px] lg:w-[600px]'>
+          <div className='w-full ring-1 ring-gray md:ring-0 rounded-md p-4 flex flex-col gap-4 px-8 md:px-0 items-center md:items-start my-4 mx-auto md:m-0 '>
+            <FaLocationDot className='text-2xl text-accent' />
+            <div className='text-lg font-bold md:text-xl lg:text-2xl'>Address</div>
+            <div>11, Seoljuk-ro 315beon-gil, Buk-gu, Gwangju, Republic of Korea (61062)</div>
+          </div>
+          <div className='w-full ring-1 ring-gray px-8 md:px-0 md:ring-0 rounded-md p-4 flex flex-col gap-2 items-center md:items-start my-4 mx-auto md:m-0 '>
+            <FaEnvelope className='text-2xl text-accent' />
+            <div className='text-lg font-bold md:text-xl lg:text-2xl'>E-mail</div>
+            <div>pennbit.korea@gmail.com</div>
+          </div>
+        </div>
+        <div className='flex justify-center items-center w-full h-[500px] mt-10'>
+          <div className='w-full h-[500px]'>
+            <LocationMap />
+          </div>
+        </div>
+      </motion.section>
     </TempLayout>
   );
 };
